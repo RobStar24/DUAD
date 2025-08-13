52e3b86634ee9e8059028a1d5cc21ebc54129ae1
Roberto Carlos <rcarlosrr2@gmail.com>
2025-08-13 12:33:05 -0600

    Minor error fixed in get_student_info.py

--- Semana 10/student_system/Utils/get_student_info.py
+++ Semana 10/student_system/Utils/get_student_info.py
@@ -13,7 +13,7 @@ def get_valid_grade(subject):
 def get_valid_name():
     while True:
         name = input("Enter student's full name: ")
-        if len(name) > 2 and all(char.isalpha() or char.isspace() for char in name)
+        if len(name) > 2 and all(char.isalpha() or char.isspace() for char in name):
             return name
         else:
             print("Error: Please enter a valid name.")
